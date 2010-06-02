30184ddd4e1bcf9c98c855041df8199ca5f1cf56
Seth W-G <seth@wytchlab.net>
2010-06-02 09:57:16 -0400

    Added license terms and copyright to all files

--- interp2_linear.c
+++ interp2_linear.c
@@ -1,4 +1,24 @@
 
+/*
+ * Copyright - 2010 - Seth Weith-Glushko
+ *
+ * This file is part of libinterp2.
+ *
+ * Libinterp2 is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as 
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ *
+ * Libinterp2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public 
+ * License along with libinterp2.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+ 
 #include "libinterp2.h"
 
 void interp2_i_linear2(INTERP2_IMAGE in, INTERP2_IMAGE out, double *x0, double *y0)
--- libinterp2.h
+++ libinterp2.h
@@ -1,8 +1,22 @@
-/* 
- * File:   libinterp2.h
- * Author: Seth
+
+/*
+ * Copyright - 2010 - Seth Weith-Glushko
  *
- * Created on April 28, 2010, 10:14 AM
+ * This file is part of libinterp2.
+ *
+ * Libinterp2 is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as 
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ *
+ * Libinterp2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public 
+ * License along with libinterp2.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #ifndef _LIBINTERP2_H
